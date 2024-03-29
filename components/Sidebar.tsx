d00eb5d1f15d3b93da06639ba9345f9c8a93f8c8
chen <565306120@qq.com>
2024-03-30 05:03:40 +1030

    arrow button finish

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,8 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
 children: React. ReactNode;
@@ -40,12 +42,26 @@ children: React. ReactNode;
           w-[300px] 
           p-2
         ">
-    <Box>
-        Sidebar Navigation
-    </Box>
+        <Box>
+        <div className="flex flex-col gap-y-4 px-5 py-4">
+                {routes.map((item) => (
+                <SidebarItem 
+                key={item.label} 
+                {...item} />
+                ))}
 
+            </div>
+        </Box>
 
+        <Box className="overflow-y-auto h-full">
+          {/* <Library songs={songs} /> */}
+          <Library />
+        </Box>
         </div>
+
+        <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+       </main>
     </div>
     );
     }
