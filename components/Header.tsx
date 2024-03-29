bb623f48b33aa53350b061dc7782cff8522f4dec
chen <565306120@qq.com>
2024-03-30 06:26:50 +1030

    supabase

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -60,7 +64,56 @@ const Header: React.FC<HeaderProps> = ({
             <RxCaretRight className="text-white" size={35} />
           </button>
             </div>
-
+            <div className="flex md:hidden gap-x-2 items-center">
+          <button 
+            onClick={() => router.push('/')} 
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button 
+            onClick={() => router.push('/search')} 
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+            <div className="flex justify-between items-center gap-x-4">
+            <div className="flex gap-x-4 items-center">
+              <Button 
+                onClick={handleLogout} 
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button 
+                onClick={() => router.push('/account')} 
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+            </div>
         </div>
         </div>
     );
