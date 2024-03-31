e164531252d41444ac3080d12a57a730d59b8ad2
chen <565306120@qq.com>
2024-04-01 06:03:03 +1030

    Final version

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,15 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
 import { FaUserAlt } from "react-icons/fa";
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
-// import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
-// import usePlayer from "@/hooks/usePlayer";
+import usePlayer from "@/hooks/usePlayer";
+
 import Button from "./Button";
-import toast from "react-hot-toast";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -23,7 +24,7 @@ const Header: React.FC<HeaderProps> = ({
   children,
   className,
 }) => {
-  // const player = usePlayer();
+  const player = usePlayer();
   const router = useRouter();
   const authModal = useAuthModal();
 
@@ -32,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
-    // player.reset();
+    player.reset();
     router.refresh();
 
     if (error) {
--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
-// import usePlayer from "@/hooks/usePlayer";
+import usePlayer from "@/hooks/usePlayer";
 
 
 interface MediaItemProps {
@@ -16,7 +16,7 @@ interface MediaItemProps {
     data,
     onClick,
   }) => {
-    // const player = usePlayer();
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
   
     const handleClick = () => {
@@ -24,7 +24,7 @@ interface MediaItemProps {
         return onClick(data.id);
       }
     
-    //   return player.setId(data.id);
+      return player.setId(data.id);
     };
 
     return ( 
